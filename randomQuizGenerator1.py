600f56ca8d85b532dd0cf3c80a42671177395d9c
Heidi Hinkel <ad-astra@comcast.net>
2021-05-11 17:25:14 -0400

    Initial Load

---
+++ randomQuizGenerator1.py
@@ -0,0 +1,26 @@
+import data.py
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+            'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
+            'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+            'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
+            'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+            'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
+            'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+            'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+            'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+            'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville',
+            'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier',
+            'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
+            'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+# TODO: Create quiz and answer key files
+# TODO: Write header for quiz
+# TODO: Shuffle the order of the states
+# TODO: Loop through the states making a question for each
+# TODO: Write the question and answer options to the quiz file.
+# TODO: Write the answer key to a file.
